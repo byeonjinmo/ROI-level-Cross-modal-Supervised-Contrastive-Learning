e04ca9a6c4f18eaa60cd57303f6b04ccecc8dcea
byeonjinmo <wlsahv@gmail.com>
2026-04-02 11:04:16 +0900

    Refactor codebase for publication: consolidate utils, fix GAT aggregation, remove legacy code

--- models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .resnet3d import MedicalNetResNet18, MedBasicBlock, Simple3DCNN, load_medicalnet_pretrained
-from .gnn import GNNBackbone
-from .multimodal_fusion import MultimodalFusion, SingleModalityModel
-from .contrastive import ContrastiveModule, InfoNCELoss, ProjectionHead, AttentionPooling
-from .roi_pooling import ROIPooling3D
-from .ot_fusion import SinkhornOT, MultimodalOTFusion
